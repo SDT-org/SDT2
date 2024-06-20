d3ac0390c2d8ec2284310ff5c16bdf1b7d3c1746
Josiah Ivey <josiah.ivey@gmail.com>
2024-06-20 16:35:12 -0700

    improve the estimated time display

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -2,27 +2,49 @@ import React from "react";
 import { AppState } from "../appState";
 import { Button, Label, ProgressBar } from "react-aria-components";
 
-export const Loader = ({ appState }: { appState: AppState }) => {
+export const Loader = ({
+  appState: { stage, progress, estimated_time },
+}: {
+  appState: AppState;
+}) => {
   const [canceling, setCanceling] = React.useState(false);
+  const [estimatedDisplay, setEstimatedDisplay] = React.useState("");
+  const updatedTime = React.useRef(Date.now());
+
+  React.useEffect(() => {
+    if (!estimated_time || Date.now() - updatedTime.current < 3000) {
+      return;
+    }
+
+    if (estimated_time < 60) {
+      setEstimatedDisplay("Estimated: <1 minute");
+      updatedTime.current = Date.now();
+      return;
+    }
+
+    const estimatedMinute = Math.round(estimated_time / 60);
+    setEstimatedDisplay(
+      `Estimated: ${estimatedMinute} minute${estimatedMinute > 1 ? "s" : ""}`,
+    );
+    updatedTime.current = Date.now();
+  }, [estimated_time]);
 
   return (
     <div className="app-wrapper">
       <div className="app-main centered loader">
-        <ProgressBar value={appState.progress}>
+        <ProgressBar value={progress}>
           {({ percentage, valueText }) => (
             <>
-              <Label>{appState.stage ? `${appState.stage}...` : null}</Label>
+              <Label>{stage ? `${stage}...` : null}</Label>
               <span className="value">{valueText}</span>
               <div className="bar">
-                <div className="fill" style={{ width: percentage + "%" }} />
+                <div
+                  className="fill"
+                  style={{ width: percentage + "%" }}
+                  data-animation={estimated_time && estimated_time > 10}
+                />
               </div>
-              <div className="estimate">
-                {appState.estimated_time ? (
-                  `Estimated: ${Math.round(appState.estimated_time / 60)} min(s)`
-                ) : (
-                  <>&nbsp;</>
-                )}
-              </div>
+              <div className="estimate">{estimatedDisplay || <>&nbsp;</>}</div>
             </>
           )}
         </ProgressBar>
