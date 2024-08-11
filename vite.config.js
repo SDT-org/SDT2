5ef4de1249bf5651fad72ed7a4d77b299763da25
Josiah Ivey <josiah.ivey@gmail.com>
2024-08-11 15:40:17 -0700

    remove unused commonjs plugin now that we use plotly.js from npm

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react(),
-    commonjs({
-      include: ["frontend/vendor/plotly-custom.min.js"],
-    }),
   ],
   build: {
     outDir: "gui",
