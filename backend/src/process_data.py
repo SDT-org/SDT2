c9a08c3dace94a68119a9ab2c3426d5ecd1077e8
Michael Lund (Student) <mclund2@asu.edu>
2024-08-29 10:48:15 -0700

    chaanged args to settings for cluster method getattr

--- backend/src/process_data.py
+++ backend/src/process_data.py
@@ -130,7 +130,7 @@ def get_alignment_scores(
 # Reorder the scores matrix based on the tree and save it to a new CSV
 def tree_clustering(settings, dm, filename):
     constructor = DistanceTreeConstructor()
-    clustering_method = getattr(constructor, args["cluster_method"])
+    clustering_method = getattr(constructor, settings["cluster_method"])
     tree_file = filename + "_tree.nwk"
     tree = clustering_method(dm)
     Phylo.write(tree, tree_file, "newick")
