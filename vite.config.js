fa53e0fe9cb2e2e40732995e33716fd6bb02aee2
Josiah Ivey <josiah.ivey@gmail.com>
2024-12-20 20:57:37 -0800

    conditionally enable react dev tools in dev mode

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ export default defineConfig(({ mode }) => ({
   root: 'frontend/src',
   plugins: [
     react(),
+    mode === 'development' && {
+     name: 'inject-devtools-script',
+     transformIndexHtml(html) {
+       return html.replace(
+         '</body>',
+         '<script src="http://localhost:8097"></script></body>'
+       );
+     },
+   },
   ],
   build: {
     outDir: "../../gui",
