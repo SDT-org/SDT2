3aad6d70a5661cc1e7753f87a48e9fae281c7417
Michael Lund (Student) <mclund2@asu.edu>
2025-06-11 11:38:22 -0700

    removed the default 16 fontsize for axis labels if sequences are less than 10- seems to fix Pierre's bug with the added effect of dropping long strings if spaced afte seq name. May be an issue but sort of using as a feature for now

--- frontend/src/plotUtils.ts
+++ frontend/src/plotUtils.ts
@@ -1,2 +1,2 @@
 export const getScaledFontSize = (base: number, count: number) =>
-  count < 10 ? 16 : Math.min(16, Math.max(1, base / (1 + count / 60)));
+  Math.min(16, Math.max(1, base / (1 + count / 60)));
