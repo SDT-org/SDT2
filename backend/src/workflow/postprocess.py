a66ebc6a21c07eba2e6b44a25eef6e654096e3b5
Michael Lund (Student) <mclund2@asu.edu>
2025-06-25 10:25:41 -0700

    parasail working

--- backend/src/workflow/postprocess.py
+++ backend/src/workflow/postprocess.py
@@ -8,10 +8,10 @@ from workflow.models import RunSettings, WorkflowResult
 
 def run(result: WorkflowResult, settings: RunSettings) -> WorkflowResult:
     doc_paths = settings.doc_paths
-    matrix, ordered_ids = result.similarity_matrix, result.ordered_ids
+    similarity_matrix, ordered_ids = 100 - result.distance_matrix, result.ordered_ids
 
     seq_stats = get_seq_stats(result.seq_dict)
-    df = DataFrame(matrix, index=ordered_ids, columns=ordered_ids)
+    df = DataFrame(similarity_matrix, index=ordered_ids, columns=ordered_ids)
     save_cols_to_csv(df, doc_paths.columns)
     save_stats_to_csv(seq_stats, doc_paths.stats)
     save_matrix_to_csv(df, doc_paths.matrix, doc_paths.triangle)
